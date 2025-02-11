74c48e604545b0bf8843d381f2fa03d86010e898
artsupawat <supawat125947@gmail.com>
2025-02-12 00:10:34 +0700

    feat(http): add HTTP handler for multiply, substract functions

--- main.go
+++ main.go
@@ -26,12 +26,52 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(AddResponse{Result: result})
 }
 
+type SubtractRequest struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+type SubtractResponse struct {
+	Result int `json:"result"`
+}
+
+func substractHandler(w http.ResponseWriter, r *http.Request) {
+	var req SubtractRequest
+	json.NewDecoder(r.Body).Decode(&req)
+
+	result := math.Subtract(req.X, req.Y)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(SubtractResponse{Result: result})
+}
+
+type MultiplyRequest struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+type MultiplyResponse struct {
+	Result int `json:"result"`
+}
+
+func multiplyHandler(w http.ResponseWriter, r *http.Request) {
+	var req MultiplyRequest
+	json.NewDecoder(r.Body).Decode(&req)
+
+	result := math.Multiply(req.X, req.Y)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(MultiplyResponse{Result: result})
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
 
 	http.HandleFunc("/add", addHandler)
+	http.HandleFunc("/substract", substractHandler)
+	http.HandleFunc("/multiply", multiplyHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
